903ce50c3c6df17c51ae4618ea6fdd69dc1967be
Isaac Berdaky <Isaac.Berdaky@escola.pr.gov.br>
2025-05-08 11:16:36 -0300

    Segundo Update

---
+++ app.js
@@ -0,0 +1,9 @@
+let f1 = parseInt(prompt("Primeiro Valor"));
+let f2 = parseInt(prompt("Segundo Valor"));
+let soma = f1 + f2
+let div = f1 / f2
+let mult = f1 * f2
+
+alert(soma)
+alert(div)
+alert(mult)
